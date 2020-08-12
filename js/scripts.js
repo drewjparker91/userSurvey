c8486854f59391c447e838a4cc9236c9811852e9
drewparker <drewjparker91@gmail.com>
2020-08-12 15:37:35 -0700

    Add form input types
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>
    Co-authored-by: Andrew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-const dob = $("#born").val();
+$(document).ready(function() {
+  $("formOne").submit(function(event) {
+    const dob = $("#born").val();
+    const name = $("#name").val();
+    const food = $("#food").val();
+    const music = $("input:radio[name=music]:checked").val();
+    const favoriteColor = $("#color").val();
+
+
+    $(.born)
+
+  });
+});
+
+
