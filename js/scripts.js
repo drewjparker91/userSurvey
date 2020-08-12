ac5ff251389640050dceacc49e7e121171b52d39
drewparker <drewjparker91@gmail.com>
2020-08-12 16:09:14 -0700

    Add js and css code
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>
    Co-authored-by: Andrew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
+
 $(document).ready(function() {
-  $("formOne").submit(function(event) {
-    const dob = $("#born").val();
-    const name = $("#name").val();
+  
+  $("#formOne").submit(function(event) {
+    const name = $("Input#name").val();
+    const dob = $("Input#born").val();
     const food = $("#food").val();
     const music = $("input:radio[name=music]:checked").val();
-    const favoriteColor = $("#color").val();
+    const favoriteColor = $("Input#color").val();
+    
+    $(".name").text(name);
+    $(".born").text(dob);
+    $(".food").text(food);
+    $(".music").text(music);
+    $(".color").text(favoriteColor);
 
+    $("#survey").show();
 
-    $(.born)
-
+    event.preventDefault();
   });
 });
 
